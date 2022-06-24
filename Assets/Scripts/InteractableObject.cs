7be22e9ce399a577a287d1195443f417b690eebc
Jack Burkhardt <43016465+jackburkhardt@users.noreply.github.com>
2022-06-23 22:55:03 -0500

    revamp outline system

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 /// <summary>
 /// An object in the world space that the player can click on to interact with.
 /// </summary>
-[RequireComponent(typeof(Collider2D))]
-//[RequireComponent(typeof(Outline))]
-public class InteractableObject : MonoBehaviour
+[RequireComponent(typeof(Outline))]
+public class InteractableObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private UnityEvent _postInteractAction;
     [SerializeField] private InteractionType _interactionType;
+    [SerializeField] private Outline _outline;
 
     [Header("For inspection")][SerializeField][TextArea] private string _flavorText;
     [Header("For travel")] [SerializeField] private Location _destination;
-    
+
     public bool PreviouslyInteractedWith { get; set; }
     public InteractionType InteractionType => _interactionType;
     public UnityEvent PostInteractAction => _postInteractAction;
@@ -24,7 +26,18 @@ public class InteractableObject : MonoBehaviour
     private void Awake()
     {
         // Just in case someone forgets to set these fields in the editor.
-        GetComponent<Collider2D>().isTrigger = true;
+        _outline = GetComponent<Outline>();
+        _outline.enabled = false;
         gameObject.layer = 3;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _outline.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _outline.enabled = false;
     }
 } 
