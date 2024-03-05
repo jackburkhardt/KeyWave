857e6122319df259a8d92f7ce448dbe009f9a9e9
austin-lopez <austinlopez2022@u.northwestern.edu>
2024-03-05 16:54:35 -0600

    restructuring + endofday.scene

---
+++ Assets/KeyWave/Runtime/Scripts/Manager/GameManager.cs
@@ -0,0 +1,210 @@
+using PixelCrushers.DialogueSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+[System.Serializable]
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameStateManager gameStateManager;
+    private PlayerEventStack playerEventStack;
+    private CustomLuaFunctions _customLuaFunctions;
+    public List<Location> locations;
+
+    public static GameState gameState;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        //if instance of GameManager already exists, destroy it
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+        
+        
+        if (gameStateManager == null)
+        {
+            gameStateManager = this.AddComponent<GameStateManager>();
+        }
+        
+        gameState = gameStateManager.gameState;
+        
+        if (playerEventStack == null)
+        {
+            playerEventStack = this.AddComponent<PlayerEventStack>();
+        }
+        _customLuaFunctions = GetComponent<CustomLuaFunctions>() ?? gameObject.AddComponent<CustomLuaFunctions>();
+
+    }
+  
+    public static void RefreshLayoutGroupsImmediateAndRecursive(GameObject root)
+
+    {
+        var componentsInChildren = root.GetComponentsInChildren<LayoutGroup>(true);
+        foreach (var layoutGroup in componentsInChildren)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(layoutGroup.GetComponent<RectTransform>());
+        }
+
+        var parent = root.GetComponent<LayoutGroup>();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(parent.GetComponent<RectTransform>());
+    }
+  
+    private void Start()
+    {
+        StartCoroutine(StartHandler());
+    }
+
+    private void OnEnable()
+    {
+       GameStateManager.OnGameStateChanged += OnGameStateChange;
+    }
+    
+    private void OnDisable()
+    {
+        GameStateManager.OnGameStateChanged -= OnGameStateChange;
+    }
+    
+    private void OnGameStateChange(GameState gameState)
+    {
+        switch (gameState.type)
+        {
+            case GameState.Type.Normal:
+                if (gameState.clock > Clock.DailyLimit)
+                {
+                    gameStateManager.SetGameStateType(GameState.Type.EndOfDay);
+                    DialogueManager.StopConversation();
+                    DialogueManager.StartConversation("EndOfDay");
+                }
+                break;
+            case GameState.Type.EndOfDay:
+                break;
+        }
+    }
+
+
+    IEnumerator StartHandler()
+    {
+        yield return StartCoroutine(playerEventStack.RunEvents());
+
+        yield return StartCoroutine(gameStateManager.LoadGameState());
+    }
+
+    public static string GetHighestDialogueNodeValue()
+    {
+        string value = string.Empty;
+        
+        var dialogueText = DialogueManager.instance.currentConversationState.subtitle.dialogueEntry.currentDialogueText;
+        var dialogueEntryTitle = DialogueManager.instance.currentConversationState.subtitle.dialogueEntry.Title;
+        var menuText = DialogueManager.instance.currentConversationState.subtitle.dialogueEntry.currentMenuText;
+        string[] textToSerialize = { dialogueText, menuText, dialogueEntryTitle };
+
+        for (int i = 0; i < textToSerialize.Length; i++)
+        {
+            if (textToSerialize[i] == string.Empty) continue;
+            value = textToSerialize[i];
+        }
+
+        return value;
+    }
+
+   
+
+    public void LoadScene(string sceneName) => StartCoroutine(LoadSceneHandler(sceneName));
+    
+    public void ChangeScene(string currentScene, string newScene) => StartCoroutine(LoadSceneHandler(newScene, currentScene));
+
+    public void OpenMap() => ChangeScene(gameStateManager.gameState.player_location, "Map");
+
+    public void EndOfDay() => ChangeScene(gameStateManager.gameState.player_location, "EndOfDay");
+    
+    public void TravelTo(string newLocation, string currentScene = "Map")
+    {
+        StartCoroutine(TravelToHandler());
+
+        IEnumerator TravelToHandler()
+        {
+            yield return StartCoroutine(LoadSceneHandler(newLocation, currentScene));
+            
+            yield return new WaitForSeconds(0.5f);
+            
+            DialogueManager.StartConversation($"{newLocation}/Base");
+        }
+    }
+    
+    public void Wait(int duration)
+    {
+       GameEvent.OnWait(duration);
+    }
+
+    public IEnumerator LoadSceneHandler(string newScene, string currentScene = "")
+    {
+        var currentSceneName = $"Resources/Scenes/{currentScene}";
+        var newSceneName = $"Resources/Scenes/{newScene}";
+        var loadingSceneName = $"Resources/Scenes/Loading";
+        var loadingScene = SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Additive);
+        
+        while (!loadingScene.isDone)
+        {
+            yield return null;
+        }
+
+        var LoadingScreen = FindObjectOfType<LoadingScreen>();
+        
+        if (LoadingScreen != null)
+        {
+            yield return StartCoroutine(LoadingScreen.FadeCanvasIn());
+        }
+        
+        if (currentScene != "") {
+        
+            var unloadCurrentScene = SceneManager.UnloadSceneAsync(currentSceneName);
+            
+            while (!unloadCurrentScene.isDone)
+            {
+                yield return null;
+            }
+        }
+        
+        var loadNewScene = SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Additive);
+        
+        while (!loadNewScene.isDone)
+        {
+            yield return null;
+        }
+        
+        if (LoadingScreen != null)
+        {
+            yield return StartCoroutine(LoadingScreen.FadeCanvasOut());
+        }
+
+        var unloadLoadingScreen = SceneManager.UnloadSceneAsync(loadingSceneName);
+        
+           while (!unloadLoadingScreen.isDone)
+           {
+               yield return null;
+           }
+    }
+    
+   
+    
+}
---
+++ Assets/KeyWave/Runtime/Scripts/Manager/GameStateManager.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameState
+{
+    public enum Type
+    {
+        Normal,
+        EndOfDay
+    }
+    
+    //default values
+    
+    public Type type = Type.Normal;
+    public int clock = 21600;
+    public string player_location = "Hotel";
+    public string current_conversation_title = string.Empty;
+    public string current_conversation_actor;
+    public string current_conversation_conversant;
+    public int current_conversation_line;
+    public string most_recent_response_node = string.Empty;
+    public List<string> lua_scripts = new List<string>();
+}
+
+public class GameStateManager : PlayerEventHandler
+{ 
+    public static GameStateManager instance;
+    public static Action<GameState> OnGameStateChanged;
+
+    protected void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public GameState gameState = new GameState();
+    protected override void OnPlayerEvent(PlayerEvents.PlayerEvent playerEvent)
+    {
+       switch (playerEvent.Type)
+            {
+                case "move":
+                    gameState.player_location = playerEvent.Value;
+                    break;
+                case "conversation_start":
+                    gameState.current_conversation_title = playerEvent.Value;
+                 //   gameState.AddLuaVariable($"{playerEvent.Value}_cycle", 0, false);
+                    break;
+                case "conversation_end":
+                    gameState.current_conversation_title = string.Empty;
+                   // gameState.IncrementLuaVariable($"{playerEvent.Value}_cycle");
+                    break;
+                case "conversation_script":
+                    gameState.lua_scripts.Add(playerEvent.Value);
+                    break;
+                case "conversation_line":
+                    gameState.current_conversation_actor = playerEvent.Sender;
+                    gameState.current_conversation_conversant = playerEvent.Receiver;
+                    gameState.current_conversation_line = int.Parse(playerEvent.Value);
+                    break;
+                case "awaiting_response":
+                    gameState.most_recent_response_node = playerEvent.Value;
+                    break;
+                case "conversation_decision":
+                    gameState.most_recent_response_node = string.Empty;
+                    break;
+                case "state_change":
+                    gameState.type = Enum.Parse<GameState.Type>(playerEvent.Value);
+                    break;
+            }
+
+            switch (gameState.type)
+            {
+                case GameState.Type.Normal:
+                    gameState.clock += playerEvent.Duration;
+                    break;
+                case GameState.Type.EndOfDay:
+                    gameState.clock = Clock.DailyLimit;
+                    break;
+            }
+        
+        OnGameStateChanged?.Invoke(gameState);
+    }
+    
+    public void SetGameStateType(GameState.Type type)
+    {
+        gameState.type = type;
+        switch (type)
+        {
+            case GameState.Type.Normal:
+                break;
+            case GameState.Type.EndOfDay:
+                gameState.clock = Clock.DailyLimit;
+                break;
+        }
+    }
+
+    public IEnumerator LoadGameState()
+    {
+        yield return GameManager.instance.LoadSceneHandler(gameState.player_location);
+        
+        //run lua scripts
+        foreach (var script in gameState.lua_scripts)
+        {
+            var parsedScript = script.Split(";");
+            foreach (var line in parsedScript)
+            {
+                if (line.Contains("ShowAlert")) continue;
+                Lua.Run(line);
+            }
+        }
+        
+        yield return new WaitForSeconds(0.5f);
+        
+        //load conversation -- order matters here
+        if (gameState.current_conversation_title != string.Empty)
+        {
+            StartCoroutine(StartConversationHandler());
+        }
+        
+        else DialogueManager.StartConversation($"{gameState.player_location}/Base");
+        
+        IEnumerator StartConversationHandler()
+        {
+            var actor = string.IsNullOrEmpty(gameState.current_conversation_actor)
+                ? null
+                : GameObject.Find(gameState.current_conversation_actor);
+            var conversant = string.IsNullOrEmpty(gameState.current_conversation_conversant)
+                ? null
+                : GameObject.Find(gameState.current_conversation_conversant);
+            var actorTransform = (actor != null) ? actor.transform : null;
+            var conversantTransform = (conversant != null) ? conversant.transform : null;
+            yield return new WaitForEndOfFrame();
+            DialogueManager.StartConversation(gameState.current_conversation_title, actorTransform, conversantTransform, gameState.current_conversation_line);
+        }
+    }
+
+}
+
+
