fe2ee6485af4ec4973c872ab36f0dc16595eea7d
austin-lopez <austinlopez2022@u.northwestern.edu>
2024-03-25 17:43:24 -0500

    restructuring + minor fixes for customresponsemenu

---
+++ Assets/KeyWave/Runtime/Scripts/DialogueSystem/CustomSequencerShortcuts.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class CustomSequencerShortcuts : MonoBehaviour
+{
+    private void Awake()
+    {
+        Sequencer.RegisterShortcut("fade", "Fade(stay, 0.5); Fade(unstay, 0.5)@1; Delay(3)");
+    }
+}
