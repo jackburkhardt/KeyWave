8f74a7b8a9eeac66d8ea84f0a9e63224aed9461e
Jack Burkhardt <mail@jackburkhardt.com>
2024-03-28 13:24:07 -0500

    add unlinked node finder tool

---
+++ Assets/Project/Editor/Scripts/Tools/UnlinkedNodeFinder.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.DialogueSystem.DialogueEditor;
+using UnityEditor;
+using UnityEngine;
+using DialogueDatabase = PixelCrushers.DialogueSystem.Wrappers.DialogueDatabase;
+
+namespace Project.Editor.Scripts.Tools
+{
+    public class UnlinkedNodeFinder : EditorWindow
+    {
+        [MenuItem("Tools/Pixel Crushers/Dialogue System/Tools/Unlinked Node Finder")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<UnlinkedNodeFinder>();
+            window.titleContent = new GUIContent("Unlinked Node Finder");
+            window.Show();
+        }
+
+        private DialogueDatabase selectedDB;
+        private Vector2 scrollPos;
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            selectedDB = EditorGUILayout.ObjectField("Dialogue Database", selectedDB, typeof(DialogueDatabase), false, GUILayout.Width(400)) as DialogueDatabase;
+
+            if (GUILayout.Button("Search", GUILayout.Width(50)))
+            {
+                if (selectedDB == null)
+                {
+                    Debug.LogError("Unlinked Node Finder: No database selected!");
+                }
+                else
+                {
+                    SearchForMissingLinks(selectedDB);   
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space(20);
+            
+            if (unlinkedNodes.Count > 0)
+            {
+                EditorGUILayout.LabelField("Unlinked nodes:");
+                scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+                foreach (var item in unlinkedNodes)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField($"{item.Converstion} ({item.Entry.subtitleText})", GUILayout.Width(400));
+                    if (GUILayout.Button("Open", GUILayout.Width(50)))
+                    {
+                        DialogueEditorWindow.OpenDialogueEntry(selectedDB, item.Entry.conversationID, item.Entry.id);
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                EditorGUILayout.EndScrollView();
+            }
+        }
+
+        private List<(string Converstion, DialogueEntry Entry)> unlinkedNodes = new();
+
+        private void SearchForMissingLinks(DialogueDatabase database)
+        {
+            unlinkedNodes.Clear();
+            foreach (var conversation in database.conversations)
+            {
+                foreach (var entry in conversation.dialogueEntries)
+                {
+                    if (entry.outgoingLinks.Count == 0 && string.IsNullOrEmpty(entry.sceneEventGuid))
+                    {
+                        //Debug.Log($"Unlinked node found: {conversation.Title} ({entry.subtitleText}) with event {entry.sceneEventGuid}");
+                        unlinkedNodes.Add((conversation.Title, entry));
+                    }
+                }
+            }
+        }
+    }
+}
