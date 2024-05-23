2895e913ab92f57e99dcf050a3b187f81faeaae9
Jack Burkhardt <mail@jackburkhardt.com>
2024-05-23 16:22:12 -0500

    reset time

--- Assets/Project/Runtime/Scripts/Manager/GameStateManager.cs
+++ Assets/Project/Runtime/Scripts/Manager/GameStateManager.cs
@@ -17,7 +17,7 @@ public class GameState
     //default values
     
     public Type type = Type.Normal;
-    public int clock = 86000;
+    public int clock = 21600;
     public int day = 1;
     public int business_score = 0;  
     public int local_savvy_score = 0;
