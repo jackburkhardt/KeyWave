0d500581b5c881832b67fdbdc31411a618167339
austin-lopez <austinlopez2022@u.northwestern.edu>
2024-12-20 08:55:24 -0600

    colorswap

---
+++ Assets/ColorSwap.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+public class ColorSwap : MonoBehaviour
+{
+    
+    
+    public List<ColorSwapData> colors;
+    private Color _defaultColor => colors[0].color;
+    
+    [Range(0, 1)]
+    public float transitionAmount = 0.5f;
+    
+    public float transitionDuration = 0.15f;
+    
+    [Serializable]
+    public class ColorSwapData
+    {
+        public string name;
+        public Color color;
+    }
+    
+    public void SetColor(string name)
+    {
+        foreach (var color in colors)
+        {
+            if (color.name == name)
+            {
+                SetColor(color.color);
+                return;
+            }
+        }
+    }
+    
+    public void SetColor(Color color)
+    {
+        var graphic = GetComponent<Graphic>();
+        if (graphic == null) return;
+        if (color == graphic.color) return;
+        if (!Application.isPlaying)
+        {
+            graphic.color = Color.Lerp(_defaultColor, color, transitionAmount);
+        }
+        else
+        {
+            var newColor = Color.Lerp(_defaultColor, color, transitionAmount);
+            DOTween.To(() => graphic.color, x => graphic.color = x, newColor, transitionDuration);
+        }
+    }
+    
+    [Button("Revert to Default")]
+    public void ResetColor()
+    {
+        SetColor(_defaultColor);
+    }
+
+    private void OnValidate()
+    {
+        if (GetComponent<Graphic>() == null) return;
+        if (colors.Count == 0)
+        {
+            colors.Add(new ColorSwapData
+            {
+                name = "Default",
+                color = GetComponent<Graphic>().color
+            });
+        }
+        else
+        {
+            colors[0].name = "Default";
+        }
+    }
+    
+    [Button]
+    private void SetDefaultColor()
+    {
+        if (colors.Find(p => p.name == "PreviousDefault") != null)
+        {
+            colors.Remove(colors.Find(p => p.name == "PreviousDefault"));
+        }
+        colors.Add(new ColorSwapData
+        {
+            name = "PreviousDefault",
+            color = colors[0].color
+        });
+        colors[0].color = GetComponent<Graphic>().color;
+        
+        
+    }
+    
+}
