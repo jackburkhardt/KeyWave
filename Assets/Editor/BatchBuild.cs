745f8cd19b49abd992f4363581011eae99b4b460
Jack Burkhardt <mail@jackburkhardt.com>
2025-01-07 17:44:27 -0600

    mostly functional build pipeline

---
+++ Assets/Editor/BatchBuild.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEditor;
+
+
+    public class BatchBuild
+    {
+        public static void DoBuildCompressed()
+        {
+            BuildPlayerOptions defaultOptions = new BuildPlayerOptions();
+            
+            PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Brotli;
+            PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.FullWithStacktrace;
+            PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Embedded;
+            PlayerSettings.SetManagedStrippingLevel(BuildTargetGroup.WebGL , ManagedStrippingLevel.Low);
+            var buildDate = DateTime.Today.ToString("yyyy.M.dd");
+            buildDate += "-DEV";
+            Console.WriteLine($"Beginning build number {buildDate}");
+            PlayerSettings.bundleVersion = buildDate;
+            PlayerSettings.SplashScreen.showUnityLogo = false;
+            
+            BuildPlayerOptions options = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(defaultOptions);
+            BuildPipeline.BuildPlayer(options);
+        }
+
+        public static void DoBuild()
+        {
+            Console.WriteLine("WTFGO");
+            BuildPlayerOptions defaultOptions = new BuildPlayerOptions();
+            
+            PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
+            PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.FullWithStacktrace;
+            PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Embedded;
+            PlayerSettings.SetManagedStrippingLevel(BuildTargetGroup.WebGL , ManagedStrippingLevel.Low);
+            var buildDate = DateTime.Today.ToString("yyyy.M.dd");
+            buildDate += "-DEV";
+            Console.WriteLine($"Beginning build number {buildDate}");
+            PlayerSettings.bundleVersion = buildDate;
+            PlayerSettings.SplashScreen.showUnityLogo = false;
+            
+            BuildPlayerOptions options = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(defaultOptions);
+            BuildPipeline.BuildPlayer(options);
+        }
+
+        public static void DoBuildRelease()
+        {
+            BuildPlayerOptions defaultOptions = new BuildPlayerOptions();
+            
+            PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Brotli;
+            PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.FullWithoutStacktrace;
+            PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.External;
+            PlayerSettings.stripEngineCode = true;
+            PlayerSettings.SetManagedStrippingLevel(BuildTargetGroup.WebGL , ManagedStrippingLevel.Medium);
+            var buildDate = DateTime.Today.ToString("yyyy.M.dd");
+            buildDate += "-PUBLIC";
+            Console.WriteLine($"Beginning build number {buildDate}");
+            PlayerSettings.bundleVersion = buildDate;
+            PlayerSettings.SplashScreen.showUnityLogo = false;
+            
+            BuildPlayerOptions options = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(defaultOptions);
+            BuildPipeline.BuildPlayer(options);
+        }
+    }
